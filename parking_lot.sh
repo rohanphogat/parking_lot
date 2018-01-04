25239e6b0020aec9509cdc043de4d863332a2332
Rohan Phogat <rohanphogat@gmail.com>
2018-01-04 19:16:12 +0530

    minor fix in shell script

--- parking_lot.sh
+++ parking_lot.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
-#!/usr/bin/env ruby
-
 chmod +x parking_lot.rb
 
 if [ -z "$1" ]
   then
     ruby parking_lot.rb
   else
-    ruby parking_lot.rb "($PWD)/$1"
+    ruby parking_lot.rb "$PWD/$1"
 fi
